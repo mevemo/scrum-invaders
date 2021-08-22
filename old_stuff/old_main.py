e4d004492e36224662865a8717be0a6adfef497a
M. Heinrich <76873680+mevemo@users.noreply.github.com>
2021-08-22 09:07:55 +0200

    und mehr ...

--- old_stuff/old_main.py
+++ old_stuff/old_main.py
@@ -2,7 +2,18 @@ import pygame
 from pygame.locals import * 
 import time
 
+class Menue:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.image = pygame.image.load("platzhalter_menue.jpg").convert()
+        self.x = 400
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
 class Game:
+    """ In dieser Klasse befinden sich alle Aufrufe der Objekte und Methoden"""
     def __init__(self):
         # Initialisiere PyGame
         pygame.init()
@@ -12,6 +23,8 @@ class Game:
 
         # Gib dem Fenster eine Beschriftung
         pygame.display.set_caption("TOLLER NAME UND SO")
+
+        self.menue = Menue(self.surface)
         
 
     def run(self):
@@ -24,6 +37,9 @@ class Game:
                     # ESC Taste setzt die Variable running auf False
                     if event.key == K_ESCAPE:
                         running = False
+                    if event.key == K_m:
+                        self.menue.draw()                # Ruft die draw() Funktion von Menue auf
+                        pygame.display.flip()            # Uebertraegt dann die Aenderung auf das Display
                     if event.key == K_w:
                         self.surface.fill((255,255,255)) # fuellt mit einer Hintergrundfarbe (weiss)
                         pygame.display.flip()            # Uebertraegt dann die Aenderung auf das Display
