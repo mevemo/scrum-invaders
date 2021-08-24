bbadf54518e20e19b1c5c600b4a6f2b5be240717
M. Heinrich <76873680+mevemo@users.noreply.github.com>
2021-08-24 16:43:14 +0200

    ENDLICH DIE BEWEGUNG GEFIXED !!!

--- game/main.py
+++ game/main.py
@@ -23,24 +23,37 @@ icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
 # Löst das Abspielen der Musik aus
-START_MUSIC.play()
+# START_MUSIC.play()
+
+# Eingabe des Spielernamens
 Spielername = input("Bitte Spielername eingeben: ") # Marcin Chris, Eingabe des Spielernames
 
-# von MAIK
-"""
+
+# Ab hier von MAIK
 class Ship:
-     Hier wird die ship-klasse definiert 
+    """ Hier wird die ship-klasse definiert """
     def __init__(self, parent_screen):
         # Erzeugt ein Viereck das die Position und Groesse des SPACESHIP speichert
         self.spieler = pygame.Rect(START_X, START_Y, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
         self.parent_screen = parent_screen
-        self.image = pygame.image.load("ship.png").convert()
+        self.image = SPACESHIP
+        self.step = 0
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.spieler.x, self.spieler.y))
         pygame.display.flip()
 
-"""
+
+    def walk(self):
+        """ Hier ist festgelegt wie sich das Ship bewegen darf """
+        if self.spieler.x + self.step > BREITE:
+            self.spieler.x = 0
+        elif self.spieler.x + self.step < 0:
+            self.spieler.x = BREITE - SPACESHIP_WIDTH
+        else:
+            self.spieler.x += self.step
+
+        self.draw()
 # bis hier von MAIK
 
 
@@ -55,13 +68,13 @@ class Game:
         # Legt Breite und Höhe des Spielfensters fest
         self.surface = pygame.display.set_mode((BREITE,HOEHE))
         
-        #self.ship = Ship(self.surface)
+        self.ship = Ship(self.surface)
 
+        # WEGEN NEU NOT NEEDED
         # Erzeugt ein Viereck das die Position und Groesse des SPACESHIP speichert
-        self.spieler = pygame.Rect(START_X, START_Y, SPACESHIP_WIDTH, SPACESHIP_HEIGHT) 
+        # self.spieler = pygame.Rect(START_X, START_Y, SPACESHIP_WIDTH, SPACESHIP_HEIGHT) 
 
-	    # Gibt dem Fenster eine Beschriftung
-        # pygame.display.set_caption(TITEL)
+	    
         # self.surface.fill((10,10,10)) # Würde eine Hintergrundfarbe bestimmen
         
 	    # pygame.display.flip()         # Uebertraegt dann die Aenderung auf das Display
@@ -78,14 +91,23 @@ class Game:
                     # ESC Taste setzt die Variable running auf False
                     if event.key == K_ESCAPE:
                         running = False
+                    # Linker Pfeil verursacht Bewegung nach links
+                    if event.key == K_LEFT:
+                        self.ship.step = -VEL
+                    # Rechter Pfeil verursacht Bewegung nach links
+                    if event.key == K_RIGHT:
+                        self.ship.step = VEL    
                         
-                keys_pressed = pygame.key.get_pressed()
+                # keys_pressed = pygame.key.get_pressed()
+
+                # NEU NOT NEEDED
                 
                 # Events durch gedrueckthalten
-                if keys_pressed[pygame.K_LEFT]: 
-	                self.spieler.x -= VEL
-                if keys_pressed[pygame.K_RIGHT]:
-	                self.spieler.x += VEL
+                # if keys_pressed[pygame.K_LEFT]: 
+	            #     self.spieler.x -= VEL
+                # if keys_pressed[pygame.K_RIGHT]:
+	            #     self.spieler.x += VEL
+                
                     
                 # Durch schließen des Fensters wird running ebenfalls auf False gesetzt
                 elif event.type == QUIT:
@@ -94,15 +116,15 @@ class Game:
             # Male das alte Bild über
             self.surface.fill((10,10,10)) # Würde eine Hintergrundfarbe bestimmen
 
-            # self.ship.draw() # von MAIK
+            self.ship.walk() # von MAIK
 
             # Zeichne das SPACESHIP
-            self.surface.blit(SPACESHIP, (self.spieler.x, self.spieler.y))
+            # self.surface.blit(SPACESHIP, (self.spieler.x, self.spieler.y))
 
             # RESRESH
             pygame.display.flip()
 
-            # Mach ma Halblang
+            # Mach ma Halblang wurde durch FPW ersetzt
             # time.sleep(0.2)
 
 
@@ -112,6 +134,4 @@ if __name__ == "__main__":
     game.run()
 
 
-    
 
-#hallo mike
