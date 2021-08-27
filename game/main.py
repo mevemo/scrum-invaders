a4b55a828a64441a33cebaf885e5b178f72b8520
Serhat Leylek <Serhat.Leylek@gfn.education>
2021-08-27 10:20:55 +0200

    Kivy Import

--- game/main.py
+++ game/main.py
@@ -6,6 +6,17 @@ import sys
 import turtle
 from turtle import textinput
 
+from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.stacklayout import StackLayout
+
 pygame.mixer.init()		# Brauchen wir fuer Sound
 
 # Hier definieren wir feste Groessen für das Gesamte Spiel
